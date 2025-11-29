07a967a9dfa363b5d21721758b1862dd7fc6a434
Matt Lant <matthewclant@gmail.com>
2025-11-29 15:03:52 -0700

    unused enum, build fix

--- src/routes/dashboard/gym.tsx
+++ src/routes/dashboard/gym.tsx
@@ -1,6 +1,5 @@
 import AuthGuard from "@/components/AuthGuard"
 import { createFileRoute, Outlet } from "@tanstack/react-router"
-import { USER_ROLE_TYPE } from "@/types/climber"
 
 export const Route = createFileRoute("/dashboard/gym")({
   component: RouteComponent,
